065544a3af43b6a1a518bee26495dfd7e9c8b3fd
Yann Thouanel <yann.thouanel@gmail.com>
2022-04-20 06:47:37 +0200

    suppression autres auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,27 +7,6 @@ const { getOnePublicationQuery } = require("../controllers/publication");
 const { getOneCommentQuery } = require("../controllers/comment");
 const db = require("../DBConnect");
 
-exports.params = (req, res, next) => {
-  // exportation identification utilisateur comparaison pour modification/suppression
-  try {
-    console.log("AUTHOR", req.headers.authorization);
-    const token = req.headers.authorization.split(" ")[1]; // extraction du token du header avec fonction split pour récupérer tout après l'espace dans le header
-    const decodedToken = jwt.verify(token, `${process.env.TOKEN}`); // verify pour décoder le token
-    const userId = parseInt(decodedToken.userId); // extraction ID utilisateur du token
-    console.log("TOKEN params", req.params.id, userId, decodedToken.userId);
-    if (req.params.id && parseInt(req.params.id) !== userId) {
-      // comparaison ID utilisateur à celui du token
-      throw "Invalid user ID";
-    } else {
-      next(); // si ok on passe à l'étape suivante
-    }
-  } catch {
-    res.status(401).json({
-      error: new Error("Invalid request!"), // si différent erreur
-    });
-  }
-};
-
 exports.isMemberOrAdminPublication = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1]; // extraction du token du header avec fonction split pour récupérer tout après l'espace dans le header
@@ -125,37 +104,6 @@ exports.token = (req, res, next) => {
     if (req.body.userId && parseInt(req.body.userId) !== userId) {
       // comparaison ID utilisateur à celui du token
       throw "Invalid user ID";
-    } else {
-      next(); // si ok on passe à l'étape suivante
-    }
-  } catch {
-    res.status(401).json({
-      error: new Error("Invalid request!"), // si différent erreur
-    });
-  }
-};
-
-exports.admin = (req, res, next) => {
-  // exportation identification utilisateur comparaison pour modification/suppression
-  try {
-    console.log("AUTHOR 3", req.headers.authorization);
-    const token = req.headers.authorization.split(" ")[1]; // extraction du token du header avec fonction split pour récupérer tout après l'espace dans le header
-    const decodedToken = jwt.verify(token, `${process.env.TOKEN}`); // verify pour décoder le token
-    const userId = parseInt(decodedToken.userId); // extraction ID utilisateur du token
-    console.log(
-      "TOKEN Body 3",
-      userId,
-      req.body.userId,
-      decodedToken.userId
-    );
-    if (req.body.userId && parseInt(req.body.userId) !== userId) {
-      // comparaison ID utilisateur à celui du token
-      throw "Invalid user ID";
-    }
-    if (req.params.id == userId) {
-      return res
-        .status(401)
-        .json({ message: "'Vous n'êtes pas autorisé à vous rajouter admin'" });
     } else {
       next(); // si ok on passe à l'étape suivante
     }
